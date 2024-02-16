9200a7e7c4eb03b1ae73b0d87997d0c86b62c793
Nicholas Collin Merritt <107739492+Subpoened-peon@users.noreply.github.com>
2024-02-15 17:08:12 -0700

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+VERSION="0.1"
+
+SOURCE_DIR="${GITHUB_WORKSPACE}"
+OUTPUT_DIR="${GITHUB_WORKSPACE}/build"
+
+# Ensure the output directory exists
+mkdir -p "${OUTPUT_DIR}"
+
+# Change directory to the source directory
+cd "${SOURCE_DIR}" || exit
+zip -r "${OUTPUT_DIR}/HookLineSecureV:$VERSION.zip ./* --exclude .github/*
+
+# additional signing required to be added later...
+
+echo "Build completed successfully!"
